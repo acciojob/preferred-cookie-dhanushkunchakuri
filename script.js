76ce9b14cf22bb8f36a008624782bc3a5e38342b
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-06-29 13:52:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-//your JS code here. If required.
-
-function getCookie(name){
-	const cookies=document.cookie.split(";");
-	for(let cook of cookies){
-		let [key, value]=cook.trim().split("=");
-		if(name === key) return value;
-	}
-	return null;
+function getCookie(name) {
+  const cookies = document.cookie.split(";");
+  for (let cook of cookies) {
+    let [key, value] = cook.trim().split("=");
+    if (name === key) return value;
+  }
+  return null;
 }
 
+window.addEventListener("DOMContentLoaded", () => {
+  let fontsize = getCookie("fontsize");
+  let fontcolor = getCookie("fontcolor");
 
-window.addEventListener("DOMContentLoaded",()=>{
-	let fontsize=getCookie("fontsize");
-	let fontcolor=getCookie("fontcolor");
-	if (fontSize) {
-    document.documentElement.style.setProperty("--fontsize", fontSize + "px");
-    document.getElementById("fontsize").value = fontSize;
+  if (fontsize) {
+    document.documentElement.style.setProperty("--fontsize", fontsize + "px");
+    document.getElementById("fontsize").value = fontsize;
   }
 
-  if (fontColor) {
-    document.documentElement.style.setProperty("--fontcolor", fontColor);
-    document.getElementById("fontcolor").value = fontColor;
+  if (fontcolor) {
+    document.documentElement.style.setProperty("--fontcolor", fontcolor);
+    document.getElementById("fontcolor").value = fontcolor;
   }
-})
+});
 
+document.getElementById("preferences-form").addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  let fontsizee = document.getElementById("fontsize").value;
+  let fontcolorr = document.getElementById("fontcolor").value;
 
+  document.cookie = `fontsize=${fontsizee}`;
+  document.cookie = `fontcolor=${fontcolorr}`;
 
-
-
-document.getElementById("preferences-form").addEventListener("submit",(e)=>{
-	e.preventDefault(); 
-	let fontsizee=document.getElementById("fontsize").value;
-	let fontcolorr=document.getElementById("fontcolor").value;
-	document.cookie = `fontsize=${fontsizee}`;
-	document.cookie = `fontcolor=${fontcolorr}`;
-
-	document.documentElement.style.setProperty("--fontsize", fontsizee + "px");
-	document.documentElement.style.setProperty("--fontcolor", fontColorr);
-})
+  document.documentElement.style.setProperty("--fontsize", fontsizee + "px");
+  document.documentElement.style.setProperty("--fontcolor", fontcolorr);
+});
