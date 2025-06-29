c7fac0bd46ea3bc0613da5ee82be983cf16eb1ff
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-06-29 13:55:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-function getCookie(name) {
-  const cookies = document.cookie.split(";");
-  for (let cook of cookies) {
-    let [key, value] = cook.trim().split("=");
-    if (name === key) return value;
-  }
-  return null;
+//your JS code here. If required.
+
+function getCookie(name){
+	const cookies=document.cookie.split(";");
+	for(let cook of cookies){
+		let [key, value]=cook.trim().split("=");
+		if(name === key) return value;
+	}
+	return null;
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  let fontsize = getCookie("fontsize");
-  let fontcolor = getCookie("fontcolor");
 
-  if (fontsize) {
-    document.documentElement.style.setProperty("--fontsize", fontsize + "px");
-    document.getElementById("fontsize").value = fontsize;
+window.addEventListener("DOMContentLoaded",()=>{
+	let fontsize=getCookie("fontsize");
+	let fontcolor=getCookie("fontcolor");
+	if (fontSize) {
+    document.documentElement.style.setProperty("--fontsize", fontSize + "px");
+    document.getElementById("fontsize").value = fontSize;
   }
 
-  if (fontcolor) {
-    document.documentElement.style.setProperty("--fontcolor", fontcolor);
-    document.getElementById("fontcolor").value = fontcolor;
+  if (fontColor) {
+    document.documentElement.style.setProperty("--fontcolor", fontColor);
+    document.getElementById("fontcolor").value = fontColor;
   }
-});
+})
 
-document.getElementById("preferences-form").addEventListener("submit", (e) => {
-  e.preventDefault();
 
-  let fontsizee = document.getElementById("fontsize").value;
-  let fontcolorr = document.getElementById("fontcolor").value;
 
-  document.cookie = `fontsize=${fontsizee}`;
-  document.cookie = `fontcolor=${fontcolorr}`;
 
-  document.documentElement.style.setProperty("--fontsize", fontsizee + "px");
-  document.documentElement.style.setProperty("--fontcolor", fontcolorr);
-});
+
+
+document.getElementById("preferences-form").addEventListener("submit",(e)=>{
+	e.preventDefault(); 
+	let fontsizee=document.getElementById("fontsize").value;
+	let fontcolorr=document.getElementById("fontcolor").value;
+	document.cookie = `fontsize=${fontsizee}`;
+	document.cookie = `fontcolor=${fontcolorr}`;
+
+	document.documentElement.style.setProperty("--fontsize", fontsizee + "px");
+	document.documentElement.style.setProperty("--fontcolor", fontcolorr);
+})
